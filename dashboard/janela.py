f118a50d5f0fb661dec774ca321ca2332422539c
Dudz <eduardoqueirozcarvalho978@gmail.com>
2026-09-16 00:18:14 -0300

    testing the dashboard adding another things

--- dashboard/janela.py
+++ dashboard/janela.py
@@ -1,12 +1,28 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit
 
 app = QApplication(sys.argv)
 
 janela = QWidget()
-janela.setWindowTitle("Minha Janela")
-janela.resize(400, 300)
+janela.setWindowTitle("StatusWatch")
+janela.resize(800, 700)
+
+layoyut = QVBoxLayout()
+
+titulo = QLabel("StatusWatch")
+titulo.setStyleSheet("font-size: 24px; font-weight: bold;")
+
+campo_url = QLineEdit()
+campo_url.setPlaceholderText("Digite a URL do site")
+
+botao_verificar = QPushButton("Verificar Status")
+
+layoyut.addWidget(titulo)
+layoyut.addWidget(campo_url)
+layoyut.addWidget(botao_verificar)
+
+janela.setLayout(layoyut)
 
 janela.show()
 
-sys.exit(app.exec_())
+app.exec()
